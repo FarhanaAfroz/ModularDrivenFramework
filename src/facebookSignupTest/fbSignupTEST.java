4295ddda9933b20692e5b413ccfdde530284afd2
Farhana Afroz <farhanany2012@gmail.com>
2017-05-28 13:12:01 -0500

    Added my first two test in fb sign up test

--- src/facebookSignupTest/fbSignupTEST.java
+++ src/facebookSignupTest/fbSignupTEST.java
@@ -1,5 +1,23 @@
 package facebookSignupTest;
 
+import org.testng.annotations.Test;
+
 public class fbSignupTEST {
      //We will be adding some test to assert the signup test in FB
+	
+	
+	@Test
+	public void fbSignup(){
+	System.out.println("fb signup test");
+		
+	}
+	
+	@Test
+	public void fbFooterlink(){
+	System.out.println("fb footer link test");
+		
+	}
+	
+	
+	
 }
